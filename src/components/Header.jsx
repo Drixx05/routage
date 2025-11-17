bd439fa34400311043674ee06c87789359d3faf9
Antonin Grenier <drixion05@gmail.com>
2025-11-17 15:12:45 +0100

    header tag in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,24 @@ import Container from "react-bootstrap/Container";
 
 export default function Header() {
 	return (
-		<Navbar expand="lg" className="bg-body-tertiary">
-			<Container>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-				<Nav className="me-auto">
-					<Nav.Link as={Link} to="/">
-						Accueil
-					</Nav.Link>
-					<Nav.Link as={Link} to="/categorie/Vêtements">
-						Vêtements
-					</Nav.Link>
-					<Nav.Link as={Link} to="/categorie/Chaussures">
-						Chaussures
-					</Nav.Link>
-				</Nav>
-			</Container>
-		</Navbar>
+		<header>
+			<Navbar expand="lg" className="bg-body-tertiary">
+				<Container>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+					<Nav className="me-auto">
+						<Nav.Link as={Link} to="/">
+							Accueil
+						</Nav.Link>
+						<Nav.Link as={Link} to="/categorie/Vêtements">
+							Vêtements
+						</Nav.Link>
+						<Nav.Link as={Link} to="/categorie/Chaussures">
+							Chaussures
+						</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</header>
 	);
 }
