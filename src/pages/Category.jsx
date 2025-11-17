9e0c0699394a101ec94c9fecc4e8f16eeb25ad60
Antonin Grenier <drixion05@gmail.com>
2025-11-17 16:59:07 +0100

    added redirect button on category pages

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,6 +1,18 @@
 import { useParams } from "react-router";
+import { useNavigate } from "react-router";
 
 export default function Category() {
 	const { category } = useParams();
-	return <h1>{category}</h1>;
+	const navigate = useNavigate();
+
+	const toHomePage = () => {
+		navigate("/");
+	};
+
+	return (
+		<div>
+			<h1>{category}</h1>
+			<button onClick={toHomePage}>Retour à la page d'accueil</button>
+		</div>
+	);
 }
