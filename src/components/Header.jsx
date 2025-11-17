b0b398752a77943fad6b53e3e6c46549f2c3368b
Antonin Grenier <drixion05@gmail.com>
2025-11-17 14:48:07 +0100

    Added Header navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
 export default function Header() {
 	return (
-		<header>
-			<p>Header</p>
-		</header>
+		<Navbar expand="lg" className="bg-body-tertiary">
+			<Container>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+				<Nav className="me-auto">
+					<Nav.Link as={Link} to="/">
+						Accueil
+					</Nav.Link>
+					<Nav.Link as={Link} to="/categorie/Vêtements">
+						Vêtements
+					</Nav.Link>
+					<Nav.Link as={Link} to="/categorie/Chaussures">
+						Chaussures
+					</Nav.Link>
+				</Nav>
+			</Container>
+		</Navbar>
 	);
 }
