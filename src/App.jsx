6729672634d1c816b60490180ee20ff8e2f77e1d
Antonin Grenier <drixion05@gmail.com>
2025-11-17 11:37:19 +0100

    added category page and route

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 		<Routes>
 			<Route element={<Layout />}>
 				<Route path="/" element={<Home />} />
+				<Route path="/categorie/:category" element={<Category />} />
 			</Route>
 		</Routes>
 	);
---
+++ src/pages/Category.jsx
@@ -0,0 +1,8 @@
+import { useParams } from "react-router";
+
+export default function Category() {
+	const { category } = useParams;
+	return (
+	<h1>{category}</h1>
+    )
+}
