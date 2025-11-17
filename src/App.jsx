01ef8bc3d060bb6c4ae52365ee7804553ae2bd75
Antonin Grenier <drixion05@gmail.com>
2025-11-17 15:10:02 +0100

    Footer styled

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 			<Route element={<Layout />}>
 				<Route path="/" element={<Home />} />
 				<Route path="/categorie/:category" element={<Category />} />
-        <Route path="*" element={<NotFound />} />
+				<Route path="*" element={<NotFound />} />
 			</Route>
 		</Routes>
 	);
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import Container from "react-bootstrap/Container";
 
 const Layout = () => {
 	return (
 		<>
 			<Header />
-			<main>
+			<Container as="main" className="py-3">
 				<Outlet />
-			</main>
+			</Container>
 			<Footer />
 		</>
 	);
